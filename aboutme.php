d0d9be26111ac053d2b4eb69c8996a4aead3eef3
Faris Ferdiansyah <63356193+faris2508@users.noreply.github.com>
2020-04-08 21:42:47 +0700

    menghapus alamat

--- aboutme.php
+++ aboutme.php
@@ -1,7 +1,6 @@
 <?php
 echo "Nama : Faris Ferdiansyah";
 echo "Asal Sekolah : SMK TARUNA BHAKTI";
-echo "Alamat : Jln.gg.belong rt 06/07 kecamatan tapos depok";
 echo "No.HP :081281969610";
 echo "Email : farisjakarta3@gmail.com";
 ?>
