cb880113e797e99b524c8caec5cc21ec03b8c4dc
Faris Ferdiansyah <63356193+faris2508@users.noreply.github.com>
2020-04-08 21:40:50 +0700

    menambahkan no.hp dan email

--- aboutme.php
+++ aboutme.php
@@ -2,4 +2,6 @@
 echo "Nama : Faris Ferdiansyah";
 echo "Asal Sekolah : SMK TARUNA BHAKTI";
 echo "Alamat : Jln.gg.belong rt 06/07 kecamatan tapos depok";
+echo "No.HP :081281969610";
+echo "Email : farisjakarta3@gmail.com";
 ?>
